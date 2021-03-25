3854e8995a85f2c19bc2843206197e7923b5a58d
ruban8877 <semalaruban.s@gmail.com>
2021-03-25 01:56:26 -0700

    updated

---
+++ src/org/sample1/DayTask.java
@@ -0,0 +1,12 @@
+package org.sample1;
+
+public class DayTask {
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		System.out.println("value of a="+a);
+		System.out.println("value of b="+b);
+		
+	}
+
+}
