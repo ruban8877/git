00988244c54bca73f29f34f13c47da687fb702a9
ruban8877 <semalaruban.s@gmail.com>
2021-03-26 00:22:07 -0700

    done

--- src/org/sample1/DayTask.java
+++ src/org/sample1/DayTask.java
@@ -4,8 +4,10 @@ public class DayTask {
 	public static void main(String[] args) {
 		int a=10;
 		int b=30;
+		int c=50;
 		System.out.println("value of a="+a);
 		System.out.println("value of b="+b);
+		System.out.println("value ofc:"+c);
 		
 	}
 
