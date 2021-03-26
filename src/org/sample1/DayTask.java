f55d953de55607ec9daba478774755bd5fac29cf
ruban8877 <semalaruban.s@gmail.com>
2021-03-26 00:28:52 -0700

    complete

--- src/org/sample1/DayTask.java
+++ src/org/sample1/DayTask.java
@@ -4,17 +4,13 @@ public class DayTask {
 	public static void main(String[] args) {
 		int a=10;
 		int b=30;
-<<<<<<< HEAD
+		String s1="ruban";
 		int c=50;
 		System.out.println("value of a="+a);
 		System.out.println("value of b="+b);
-		System.out.println("value ofc:"+c);
-=======
-		String s1="ruban";
-		System.out.println("value of a="+a);
-		System.out.println("value of b="+b);
 		System.out.println("Name:"+s1);
->>>>>>> 5a3c849150ef59224f1562a4a779b87bca6724ae
+		System.out.println("value ofc:"+c);
+
 		
 	}
 
